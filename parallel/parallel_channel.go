19d64d01e3ac9b58b21431650946225c7b62cfcd
zhangzheyuan <zhangzheyuan@zuoyebang.com>
2019-09-17 19:01:12 +0800

    unfinished parallel demos

---
+++ parallel/parallel_channel.go
@@ -0,0 +1,23 @@
+// 进一步理解channel未满时的非阻塞状态
+package main
+
+import "fmt"
+
+func fibonacci(n int, c chan int) {
+	x, y := 0,1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x + y
+	}
+	close(c) // 通知本次填值结束，不应再从此channel中读值
+}
+
+func main()  {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	go func() {fmt.Println("loop start")}()
+	for i := range c {
+		fmt.Println(i)
+	}
+	fmt.Println("loop end")
+}
